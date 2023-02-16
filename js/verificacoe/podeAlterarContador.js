8557cc4460988346ae867253079312a1d0faea21
Bruno Dias <dias.desenvolvedor@gmail.com>
2023-02-16 13:18:25 -0300

    BUG - redução de ponstos de pois de gastar todos

--- js/verificacoe/podeAlterarContador.js
+++ js/verificacoe/podeAlterarContador.js
@@ -5,9 +5,9 @@ function podeAlterarContador(operacao, pontosDiponiveis, contador){
     if (operacao == 'subtrair' && parseInt(pontosDiponiveis.value) == 40) {
         return false
     }
-    if (operacao == 'subtrair' && parseInt(pontosDiponiveis.value) == 0 ) {
-        return false
-    }
+    // if (operacao == 'subtrair' && parseInt(pontosDiponiveis.value) == 0 ) {
+    //     return false
+    // }
     if (operacao == 'subtrair' && parseInt(contador.value) == 0) {
         return false
     }
